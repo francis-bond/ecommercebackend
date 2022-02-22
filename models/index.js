1cd457a7947c3f5ee78264c5d25a4550cadcdaea
Francis Bond <francisbond1996@gmail.com>
2022-02-22 10:11:21 -0600

    added relationships of my models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsTo(Tag, {
+  through: 'ProductTag',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsTo(Product, {
+  through: 'ProductTag',
+});
 
 module.exports = {
   Product,
